c0b08ec6ebd8b60eda723c86468cbb95cc3cd3a6
megadox <dhappymean@gmail.com>
2021-05-19 13:44:35 +0900

    Add project files.

---
+++ EvernoteClone/ViewModel/Commands/NewNotebookCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Windows.Input;
+
+namespace EvernoteClone.ViewModel.Commands
+{
+    public class NewNotebookCommand : ICommand
+    {
+        public NotesVM VM { get; set; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public NewNotebookCommand(NotesVM vm)
+        {
+            VM = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //TODO : Create new notebook
+        }
+    }
+}
---
+++ EvernoteClone/ViewModel/NotesVM.cs
@@ -0,0 +1,39 @@
+﻿using EvernoteClone.Model;
+using EvernoteClone.ViewModel.Commands;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace EvernoteClone.ViewModel
+{
+    public class NotesVM
+    {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
+		private Notebook selectedNotebook;
+
+		public Notebook SelectedNotebook
+		{
+			get { return selectedNotebook; }
+			set 
+			{ 
+				selectedNotebook = value; 
+				//TODO : get notes
+			}
+		}
+
+		public ObservableCollection<Note> Notes { get; set; }
+
+		public NewNotebookCommand NewNotebookCommand { get; set; }
+
+		public NewNoteCommand NewNoteCommand { get; set; }
+
+		public NotesVM()
+		{
+			NewNotebookCommand = new NewNotebookCommand(this);
+			NewNoteCommand = new NewNoteCommand(this);
+		}
+
+	}
+}
