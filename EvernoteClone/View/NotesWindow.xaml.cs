678ea4a7947845e5935d38854b57dc8818e216ff
megadox <dhappymean@gmail.com>
2021-05-21 09:17:57 +0900

    Implement Speech functionality

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -9,6 +9,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Linq;
+using System.Threading;
+
 
 namespace EvernoteClone.View
 {
@@ -17,9 +22,32 @@ namespace EvernoteClone.View
     /// </summary>
     public partial class NotesWindow : Window
     {
+        SpeechRecognitionEngine recognizer;
+
         public NotesWindow()
         {
             InitializeComponent();
+
+            //var currentCulture = (from r in SpeechRecognitionEngine.InstalledRecognizers()
+            //                     where r.Culture.Equals(Thread.CurrentThread.CurrentCulture)
+            //                     select r).FirstOrDefault();
+
+
+            //recognizer = new SpeechRecognitionEngine(Thread.CurrentThread.CurrentCulture);
+
+            //GrammarBuilder builder = new GrammarBuilder();
+            //builder.AppendDictation();
+            //Grammar grammar = new Grammar(builder);
+            //recognizer.LoadGrammar(grammar);
+            //recognizer.SetInputToDefaultAudioDevice();
+            //recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
+            
+        }
+
+        private void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string recognizeText = e.Result.Text;
+            contentRichTextBox.Document.Blocks.Add(new Paragraph(new Run(recognizeText)));
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -27,9 +55,36 @@ namespace EvernoteClone.View
             Application.Current.Shutdown();
         }
 
+        bool isRecognize = false;
+
         private void SpeechButton_Click(object sender, RoutedEventArgs e)
         {
+            //Azure Server 이용  : 나중에 구현
+             
+            // Implement with .Net Framework Speech Reconizer
+            if(!isRecognize)
+            {
+                recognizer.RecognizeAsync(RecognizeMode.Multiple);
+                isRecognize = true;
+            }
+            else
+            {
+                recognizer.RecognizeAsyncStop();
+                isRecognize = false;
+            }
+            
+        }
 
+        private void contentRichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int amountCharacters = (new TextRange(contentRichTextBox.Document.ContentStart, contentRichTextBox.Document.ContentEnd)).Text.Length;
+
+            statusTextBox.Text = $"Document length : {amountCharacters} characters"; 
+        }
+
+        private void boldButton_Click(object sender, RoutedEventArgs e)
+        {            
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontWeightProperty, FontWeights.Bold);
         }
     }
 }
